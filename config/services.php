999cee0f2f58d2aa1d9c90ae8061c1af7db38a36
Souei08 <jubet.sode.5@gmail.com>
2023-11-30 00:45:10 +0800

    updates email

--- config/services.php
+++ config/services.php
@@ -30,11 +30,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
-    'sendinblue' => [
-        // api-key or partner-key
-        'key_identifier' => env('SENDINBLUE_KEY_IDENTIFIER', 'api-key'),
-        'key' => env('SENDINBLUE_KEY'),
-    ],
+    // 'sendinblue' => [
+    //     // api-key or partner-key
+    //     'key_identifier' => env('SENDINBLUE_KEY_IDENTIFIER', 'api-key'),
+    //     'key' => env('SENDINBLUE_KEY'),
+    // ],
 
     'twilio' => [
         'account_sid' => env('TWILIO_ACCOUNT_S_ID', ''),
